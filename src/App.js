afe4619fa3748df78cde9527ab6f3f7abb9184af
Kshitij <kshitij.mahajan.20042@iitgoa.ac.in>
2023-07-04 16:40:25 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from 'axios';
+import { NFTStorage } from "nft.storage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const [prompt, setPrompt] = useState("");
+  const [address, setAddress] = useState("");
+
+  //console.log(prompt)
+
+  const [imageBlob, setImageBlob] = useState(null)
+  const [file, setFile] = useState(null)
+
+const generateArt = async () => {
+	try {
+		const response = await axios.post(
+			`https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5`,
+			{
+				headers: {
+					Authorization: `Bearer ${process.env.REACT_APP_HUGGING_FACE}}`,
+				},
+				method: "POST",
+				inputs: prompt,
+			},
+			{ responseType: "blob" }
+		);
+		// convert blob to a image file type
+		const file = new File([response.data], "image.png", {
+			type: "image/png",
+		});
+		// saving the file in a state
+		setFile(file);
+		const url = URL.createObjectURL(response.data);
+		// console.log(url)
+		console.log(url);
+		setImageBlob(url);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const cleanupIPFS = (url) => {
+  if(url.includes("ipfs://")) {
+    return url.replace("ipfs://", "https://ipfs.io/ipfs/")
+  }
+}
+
+
+const uploadArtToIpfs = async () => {
+  try {
+
+    const nftstorage = new NFTStorage({
+			token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEFCODJjNTgwQTJjNGU5ZmFBNDZBZkJlNDU5OTgyZmMyNGNEYjU0NzkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY4ODQ2NDI2ODM5MywibmFtZSI6IkRlbW8ifQ.DnJ3fq5ru59WNvzRGJEyItsRilfcwulG2cKAO6I4CdQ",
+		})
+
+    const store = await nftstorage.store({
+      name: "AI NFT",
+      description: "AI generated NFT",
+      image: file
+    })
+    console.log("done");
+    return cleanupIPFS(store.data.image.href)
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+const mintNft = async () => {
+	try {
+		const imageURL = await uploadArtToIpfs();
+
+		// mint as an NFT on nftport
+		const response = await axios.post(
+			`https://api.nftport.xyz/v0/mints/easy/urls`,
+			{
+				file_url: imageURL,
+				chain: "polygon",
+				name: "Sample NFT",
+				description: "Build with NFTPort!",
+				mint_to_address: address,
+			},
+      {
+        headers: {
+          Authorization: 'e4b76b6e-4bc2-4ed8-8638-d8b6ac5a9402',
+        }
+      }
+		);
+		const data = await response.data;
+		console.log(data);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+
+return (
+	<div className="flex flex-col items-center justify-center min-h-screen gap-4">
+		<h1 className="text-4xl font-extrabold">AI Art Gasless mints</h1>
+    <div className="flex flex-col items-center justify-center">
+      {/* Create an input box and button saying next beside it */}
+      <div className="flex items-center justify-center gap-4">
+        <input
+          className="border-2 border-black rounded-md p-2 leading-8 my-4"
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text"
+          placeholder="Enter a prompt"
+        />
+        <button onClick={generateArt} className="bg-black text-white rounded-md p-2">Next</button>       
+      </div>
+      {imageBlob && (
+	<div className="flex flex-col gap-4 items-center justify-center">
+		<img src={imageBlob} alt="AI generated art" />
+    <input
+      className="border-2 border-black rounded-md p-2 leading-8 my-4"
+      onChange={(e) => setAddress(e.target.value)}
+      type="text"
+      placeholder="Enter an Address"
+    />
+		<button
+			onClick={mintNft}
+			className="bg-black text-white rounded-md p-2"
+		>
+			SEND GIFT
+		</button>
+	</div>
+)}
     </div>
-  );
+	</div>
+);
+
 }
 
 export default App;
